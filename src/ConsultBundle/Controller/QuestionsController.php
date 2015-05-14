6e0419572e66306d2f5f3f1848775b7dbe2c6e54
sabhat-practo <sahana.bhat@practo.com>
2015-05-14 12:20:31 +0000

    Merging changes into v1

--- src/ConsultBundle/Controller/QuestionsController.php
+++ src/ConsultBundle/Controller/QuestionsController.php
@@ -31,10 +31,6 @@ class QuestionsController extends Controller
             return View::create(json_decode($e->getMessage(),true), Codes::HTTP_BAD_REQUEST);
         }
 
-        //$router = $this->get('router');
-        //$patientGrowthURL = $router->generate('get_patientgrowth', array(
-        //    'patientGrowthId' => $patientGrowth->getId()), true);
-
         return View::create(
             $question,
             Codes::HTTP_CREATED);
@@ -45,14 +41,18 @@ class QuestionsController extends Controller
      *
      * @param integer $question
      *
-     *
      * @return Array
      *
      */
-    public function getQuestionAction($questionId)
+    public function getQuestionAction()
     {
         $questionManager = $this->get('consult.question_manager');
-        $request = $this->getRequest();
+        $requestData = $this->getRequest()->query->all();
+        if (!array_key_exists('question_id', $requestData)) {
+            return View::create(null, Codes::HTTP_BAD_REQUEST);
+        }
+
+        $questionId = $requestData['question_id'];
         try {
             $question = $questionManager->load($questionId);
         } catch (AccessDeniedException $e) {
@@ -67,44 +67,81 @@ class QuestionsController extends Controller
         return $question;
     }
 
-    /**
-     * Get Question by UserID Action
-     *
-     * @param integer $practoid
-     *
-     *
-     * @return Array
-     *
-     */
-    public function getQuestionUseridAction($practoId)
-    {
-        $questionManager = $this->get('consult.question_manager');
-        $request = $this->getRequest();
-        try {
-            $questionList = $questionManager->loadByUserID($practoId);
-        } catch (AccessDeniedException $e) {
-            return View::create($e->getMessage(), Codes::HTTP_FORBIDDEN);
-        }
-        if (null === $questionList) {
-            return View::create(null, Codes::HTTP_NOT_FOUND);
-        } 
-
-        return $questionList;
-    }
-
     public function getQuestionsAction()
     {
         $questionManager = $this->get('consult.question_manager');
-        $request = $this->getRequest();
+        $request = $this->getRequest()->query->all();
         try {
-            $questionList = $questionManager->loadAll();
+            if (empty($request)) {
+                $questionList = $questionManager->loadAll();
+            } else {
+                if (array_key_exists('practo_account_id', $request) and !array_key_exists('bookmarks', $request)) {
+                    $questionList = $questionManager->loadByAccId($request['practo_account_id']);
+                }
+                if (array_key_exists('bookmarks', $request) and $request['bookmarks'] === 'true' and array_key_exists('practo_account_id', $request)) {
+                    $bookmarkList = $questionManager->loadBookmarksById($request['practo_account_id']);
+                }
+                if (array_key_exists('modified_at', $request)) {
+                    $questionList = $questionManager->loadByModifiedTime(new \DateTime('now'));
+                }
+                if (array_key_exists('state', $request)) {
+                    $limit = 100;
+                    $offset = 0;
+                    if (array_key_exists('limit', $request)) {
+                        $limit = $request['limit'];
+                    }
+                    if (array_key_exists('offset', $request)) {
+                        $offset = $request['offset'];
+                    }
+                    $questionList = $questionManager->loadFeed($request['state'], $limit, $offset);
+                }
+            }
         } catch (AccessDeniedException $e) {
             return View::create($e->getMessage(), Codes::HTTP_FORBIDDEN);
         }
-        if (null === $questionList) {
+
+/*        if (null === $questionList) {
             return View::create(null, Codes::HTTP_NOT_FOUND);
         }
+*/
+        if (!empty($questionList)) {
+            return $questionList;
+        }
+        if (!empty($bookmarkList)) {
+            return $bookmarkList;
+        }
+        return View::create(null, Codes::HTTP_NOT_FOUND);
+    }
 
-        return array("questions" => $questionList);
+    public function patchQuestionAction()
+    {
+        $questionManager = $this->get('consult.question_manager');
+        $request = $this->getRequest()->request->all();
+
+        if (array_key_exists('question_id', $request)) {
+            try {
+                $question = $questionManager->load($request['question_id']);
+            } catch (AccessDeniedException $e) {
+                return View::create($e->getMessage(), Codes::HTTP_FORBIDDEN);
+            }
+            if (null === $question) {
+                return View::create(null, Codes::HTTP_NOT_FOUND);
+            } else if ($question->isSoftDeleted()) {
+                return View::create(null, Codes::HTTP_GONE);
+            }
+        } else {
+            return View::create(null, Codes::HTTP_BAD_REQUEST);
+        }
+
+        try {
+            $questionFinal = $questionManager->patch($question, $request);
+        } catch (ValidationError $e) {
+            return View::create(json_decode($e->getMessage(),true), Codes::HTTP_BAD_REQUEST);
+        }
+
+        return $questionList;
+        return View::create(
+            $questionFinal,
+            Codes::HTTP_CREATED);
     }
 }
