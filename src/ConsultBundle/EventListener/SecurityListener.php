d6c4250298386990f6d73a5284d75b54455f22e2
anshu0806 <anshu.cool@gmail.com>
2015-05-18 16:06:31 +0000

    Adding authentication changes

--- src/ConsultBundle/EventListener/SecurityListener.php
+++ src/ConsultBundle/EventListener/SecurityListener.php
@@ -6,6 +6,74 @@
  * Time: 11:53
  */
 
+namespace ConsultBundle\EventListener;
+
+use ConsultBundle\Utility\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
 class SecurityListener {
+
+    private $authenticationUtils;
+
+    public function __construct(AuthenticationUtils $authenticationUtils)
+    {
+        $this->authenticationUtils = $authenticationUtils;
+    }
+
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        //var_dump("123");
+
+
+        if ($request->getMethod() === 'GET') {
+            //var_dump("GET");
+
+            return;
+        }
+
+        $session = $request->getSession();
+
+        if ($session->get("isValidated") === true) {
+
+            return;
+        }
+
+
+        $profileToken = $request->headers->get('X-PROFILE-TOKEN');
+        $practoAccountID = $request->get('practo_account_id');
+        //var_dump(is_null($profileToken||$practoAccountID));
+        if (is_null($profileToken) || is_null($practoAccountID)) {
+            $responseRet = new Response();
+            $responseRet->setStatusCode(Response::HTTP_FORBIDDEN);
+            $event->setResponse($responseRet);
+
+        }
+
+        $this->authenticationUtils->authenticateWithAccounts($practoAccountID, $profileToken);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- src/ConsultBundle/Utility/AuthenticationUtils.php
+++ src/ConsultBundle/Utility/AuthenticationUtils.php
@@ -9,6 +9,80 @@
 namespace ConsultBundle\Utility;
 
 
+use Buzz\Browser;
+use Buzz\Bundle\BuzzBundle\Buzz\BrowserManager;
+use Buzz\Bundle\BuzzBundle\Buzz\Buzz;
+use GuzzleHttp\Client;
+use PhpCollection\Map;
+
 class AuthenticationUtils {
+
+
+    private static  $authenticationMap;
+    private $browser;
+
+
+
+    public function __construct()
+    {
+        if(AuthenticationUtils::$authenticationMap ===null)
+        {
+            AuthenticationUtils::$authenticationMap = new Map();
+        }
+
+
+    }
+
+    /**
+     * @param $practoAccountId
+     * @param $profileToken
+     * @return bool
+     */
+    public function authenticateWithAccounts($practoAccountId, $profileToken)
+    {
+        if($this->isAlreadyValidated($practoAccountId, $profileToken))
+            return true;
+
+        return $this->validateWithTokenNew($practoAccountId, $profileToken);
+    }
+
+    /**
+     * @param $practoAccountId
+     * @param $profileToken
+     * @return bool
+     */
+    private function isAlreadyValidated($practoAccountId, $profileToken)
+    {
+        return $profileToken === AuthenticationUtils::$authenticationMap->get($practoAccountId);
+    }
+
+    /**
+     * @param $practoAccountId
+     * @param $profileToken
+     * @return bool
+     */
+    private function validateWithToken($practoAccountId, $profileToken)
+    {
+        var_dump($profileToken);die;
+        //$browser = new Browser();
+        //$browser->h
+        //$response = $this->browser->get("http://accounts-consult.practodev.com".'/get_profile_with_token', ['X-Profile-Token' => $profileToken]);
+        //$client = new Client(["base_url" => "https://accounts-consult.practodev.com", 'defaults' => ['headers' => ['X-Profile-Token' => $profileToken]]]);
+        //$response = $client->get('/get_profile_with_token');
+        //var_dump($response->getHeader("content-type"));die;
+        return false;
+    }
+
+
+    private function validateWithTokenNew($practoAccountId, $profileToken)
+    {
+        $client = new Client([
+            'base_url' => 'http://accounts-consult.practodev.com',
+            'defaults' => [
+                'headers' => ['X-Profile-Token' => $profileToken]
+            ]]);
+        $res = $client->get('/get_profile_with_token');
+
+    }
 
 }
