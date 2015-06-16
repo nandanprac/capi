72fd48f3b2f77fe3d325f228117ec49b3cdaac43
sabhat-practo <sahana.bhat@practo.com>
2015-06-16 06:41:46 +0000

    Fixing comment query

--- src/ConsultBundle/Repository/QuestionCommentRepository.php
+++ src/ConsultBundle/Repository/QuestionCommentRepository.php
@@ -42,57 +42,22 @@ class QuestionCommentRepository extends EntityRepository
             ->setMaxResults($limit)
             ->setFirstResult($offset);
 
-        $commentList = $qb->getQuery()->getArrayResult();
-
-        // query for getting vote details for a particulat practo_account_id
-        if (!empty($practoAccountId)) {
-            $qb1 = $this->_em->createQueryBuilder();
-            $qb1->select('c.id', 'CASE
-                            WHEN cv.practoAccountId = :practoAccountId then cv.vote
-                            ELSE 0
-                                END as has_voted')
-                ->from(ConsultConstants::QUESTION_COMMENT_VOTE_ENTITY_NAME, 'cv')
-                ->where('cv.practoAccountId = :practoAccountId')
-                ->leftJoin(ConsultConstants::QUESTION_COMMENT_ENTITY_NAME, 'c', 'WITH', 'c = cv.questionComment')
-            ->setParameter('practoAccountId', $practoAccountId);
-
-            $voteList = $qb1->getQuery()->getArrayResult();
-
-            $refinedComments = array();
-            foreach ($commentList as $comment) {
-                $comment = $this->mergeVote($voteList, $comment);
-                array_push($refinedComments, $comment);
-            }
+        if(!empty($practoAccountId)) {
+            $qb->addSelect('COALESCE(cv1.vote, 0) as has_voted')
+               ->leftJoin(ConsultConstants::QUESTION_COMMENT_VOTE_ENTITY_NAME, 'cv1', 'WITH', 'c = cv1.questionComment and cv1.practoAccountId = :practoAccountId and cv1.softDeleted = 0')
+               ->setParameter('practoAccountId', $practoAccountId);
         }
 
+        $commentList = $qb->getQuery()->getArrayResult();
 
-        //paginator cannot be used to retrieve count of queries where more than one table is involved
         $countQuery = $qb->getQuery();
         $countQuery->setFirstResult(null)->setMaxResults(null);
         $count =  count($countQuery->getArrayResult());
 
-        if (!empty($refinedComments)) {
-            $comments = $refinedComments;
-        } else if (!empty($commentList)) {
-            $comments = $commentList;
-        } else {
+        if (empty($commentList)) {
             return null;
         }
 
-        return array('comments' => $comments, 'count' => $count);
-    }
-
-    private function mergeVote($voteList, $comment)
-    {
-        foreach ($voteList as $vote) {
-            if ($vote['id'] == $comment['id']) {
-                $comment['has_voted'] = $vote['has_voted'];
-
-                return $comment;
-            }
-        }
-        $comment['has_voted'] = 0;
-
-        return $comment;
+        return array('comments' => $commentList, 'count' => $count);
     }
 }
