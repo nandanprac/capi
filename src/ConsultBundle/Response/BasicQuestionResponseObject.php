7a33a6fbc2d278c76f5c07fa12c1074bc0a00f57
anshu0806 <anshu.cool@gmail.com>
2015-06-19 06:17:42 +0000

    Adding state in detail question

--- src/ConsultBundle/Response/BasicQuestionResponseObject.php
+++ src/ConsultBundle/Response/BasicQuestionResponseObject.php
@@ -69,7 +69,7 @@ class BasicQuestionResponseObject extends ConsultResponseObject
         if (!is_null($questionEntity)) {
             $this->setSpeciality($questionEntity->getSpeciality());
             $this->setViewCount($questionEntity->getViewCount());
-            //$this->setShareCount($questionEntity->getSpeciality());
+            $this->state = $questionEntity->getState();
             $this->setSubject($questionEntity->getSubject());
             $this->setText($questionEntity->getText());
             $this->viewedAt = $questionEntity->getViewedAt();
